5589371329463e7490b94a3aa2304c0acba9d38b
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-06-03 05:59:56 +0300

    DB-02 Add initial admin seed script

---
+++ SQL/Migrations/02_SeedInitialAdmin.sql
@@ -0,0 +1,28 @@
+﻿USE SupportTicketDB;
+GO
+
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM Users
+    WHERE Email = 'admin@support.com'
+)
+BEGIN
+    INSERT INTO Users
+    (
+        FullName,
+        Email,
+        PasswordHash,
+        Role,
+        IsActive
+    )
+    VALUES
+    (
+        'Ahmed Gamal',
+        'admin@support.com',
+        '$2a$11$8i8gQ0yEdckTfm79aKR/Xe5ffs.XQsF5INMAINt.TgCLbbddSpGqi',
+        'Admin',
+        1
+    );
+END
+GO
