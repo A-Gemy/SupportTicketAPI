01c9fe2585cb53bcec16fd3c66e94b16c6e17ebe
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-06-03 16:12:29 +0300

    DB-03 Add customer registration procedure

---
+++ SQL/Migrations/03_AddRegisterCustomerProcedure.sql
@@ -0,0 +1,49 @@
+﻿USE SupportTicketDB;
+GO
+
+CREATE OR ALTER PROCEDURE usp_RegisterCustomer
+    @FullName NVARCHAR(100),
+    @Email NVARCHAR(150),
+    @PasswordHash NVARCHAR(500)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF EXISTS
+    (
+        SELECT 1
+        FROM Users
+        WHERE Email = @Email
+    )
+    BEGIN
+        SELECT 
+            CAST(0 AS BIT) AS IsSuccess,
+            'Email already exists.' AS Message,
+            NULL AS UserId;
+
+        RETURN;
+    END
+
+    INSERT INTO Users
+    (
+        FullName,
+        Email,
+        PasswordHash,
+        Role,
+        IsActive
+    )
+    VALUES
+    (
+        @FullName,
+        @Email,
+        @PasswordHash,
+        'Customer',
+        1
+    );
+
+    SELECT 
+        CAST(1 AS BIT) AS IsSuccess,
+        'Customer registered successfully.' AS Message,
+        SCOPE_IDENTITY() AS UserId;
+END
+GO
