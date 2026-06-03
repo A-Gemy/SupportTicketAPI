0db08334db9e2e4f0d808b2b4722436e4c7b8076
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-06-03 04:17:59 +0300

    AUTH-01 Add auth models and password hashing

---
+++ Constants/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace SupportTicketAPI.Constants
+{
+    public static class UserRoles
+    {
+        public const string Customer = "Customer";
+
+        public const string Agent = "Agent";
+
+        public const string Admin = "Admin";
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using SupportTicketAPI.Security;
+using SupportTicketAPI.Services.Interfaces;
 
 namespace SupportTicketAPI
 {
@@ -13,6 +15,8 @@ namespace SupportTicketAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
             var app = builder.Build();
 
