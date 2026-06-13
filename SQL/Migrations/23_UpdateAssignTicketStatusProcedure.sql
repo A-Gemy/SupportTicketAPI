7b0bbf01af4755a25bc476bbcc8305bd5e363d5f
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-06-13 21:21:19 +0300

    DB-23 Set assigned status when assigning ticket

---
+++ SQL/Migrations/23_UpdateAssignTicketStatusProcedure.sql
@@ -0,0 +1,84 @@
+﻿USE SupportTicketDB;
+GO
+
+CREATE OR ALTER PROCEDURE usp_AssignTicketToAgent
+    @AdminId INT,
+    @TicketId INT,
+    @AgentId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM Users
+        WHERE UserId = @AdminId
+          AND Role = 'Admin'
+          AND IsActive = 1
+    )
+    BEGIN
+        SELECT
+            CAST(0 AS BIT) AS IsSuccess,
+            'Admin not found or inactive.' AS Message;
+
+        RETURN;
+    END
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM Tickets
+        WHERE TicketId = @TicketId
+    )
+    BEGIN
+        SELECT
+            CAST(0 AS BIT) AS IsSuccess,
+            'Ticket not found.' AS Message;
+
+        RETURN;
+    END
+
+    IF EXISTS
+    (
+        SELECT 1
+        FROM Tickets
+        WHERE TicketId = @TicketId
+          AND Status = 'Closed'
+    )
+    BEGIN
+        SELECT
+            CAST(0 AS BIT) AS IsSuccess,
+            'Closed tickets cannot be assigned.' AS Message;
+
+        RETURN;
+    END
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM Users
+        WHERE UserId = @AgentId
+          AND Role = 'Agent'
+          AND IsActive = 1
+    )
+    BEGIN
+        SELECT
+            CAST(0 AS BIT) AS IsSuccess,
+            'Agent not found or inactive.' AS Message;
+
+        RETURN;
+    END
+
+    UPDATE Tickets
+    SET
+        AssignedAgentId = @AgentId,
+        Status = 'Assigned',
+        UpdatedAt = SYSUTCDATETIME()
+    WHERE TicketId = @TicketId;
+
+    SELECT
+        CAST(1 AS BIT) AS IsSuccess,
+        'Ticket assigned to agent successfully.' AS Message;
+END
+GO
