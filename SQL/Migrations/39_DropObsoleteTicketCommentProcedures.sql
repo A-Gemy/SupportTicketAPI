ac9ef0c8cdbea2d1db5c0b5bddc056fc5a98a5a3
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-06-17 21:16:39 +0300

    DB-39 Remove obsolete ticket comment procedures

---
+++ SQL/Migrations/39_DropObsoleteTicketCommentProcedures.sql
@@ -0,0 +1,11 @@
+﻿USE SupportTicketDB;
+GO
+
+DROP PROCEDURE IF EXISTS dbo.usp_AddCustomerTicketComment;
+GO
+
+DROP PROCEDURE IF EXISTS dbo.usp_AddAdminTicketComment;
+GO
+
+DROP PROCEDURE IF EXISTS dbo.usp_GetCustomerTicketComments;
+GO
